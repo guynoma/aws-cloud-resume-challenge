ac3114c7dbf2ee5be8cbab2dd3c30902404c56bc
guynoma <mucaifred@yahoo.com>
2024-09-07 18:36:53 +0300

    another html edit to highlight section within projects

--- Downloads/cloud-resume-challenge/fred_resume/index.js
+++ Downloads/cloud-resume-challenge/fred_resume/index.js
@@ -1,6 +1,5 @@
-
-window.onload = async function getVisitCount() {
-	const response = await fetch('https://wh6pggpz7ewoaviwnzylv5fpy40ushtx.lambda-url.us-east-1.on.aws/') // Use const instead of let
-	const data = await response.json() // Use const instead of let
-	document.getElementById('visit-count').innerText = data.value
-  }
+window.onload = async function getVisitCount () {
+  const response = await fetch('https://wh6pggpz7ewoaviwnzylv5fpy40ushtx.lambda-url.us-east-1.on.aws/') // Use const instead of let
+  const data = await response.json() // Use const instead of let
+  document.getElementById('visit-count').innerText = data.value
+}
--- Downloads/cloud-resume-challenge/fred_resume/js/scripts.js
+++ Downloads/cloud-resume-challenge/fred_resume/js/scripts.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const elements = document.querySelectorAll('.needs-validation')
+  const elements = document.querySelectorAll('.needs-validation')
 
-    Array.prototype.slice.call(elements)
-      .map(function (element) {
-        element.addEventListener('submit', function (event) {
-          if (!element.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-          element.classList.add('was-validated')
-        }, false)
-      })
+  Array.prototype.slice.call(elements)
+    .map(function (element) {
+      element.addEventListener('submit', function (event) {
+        if (!element.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        element.classList.add('was-validated')
+      }, false)
+    })
 
-    // Initialize Bootstrap popover
-    new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'))
-  })
-
-
+  // Initialize Bootstrap popover
+  new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'))
+})
